86166bee2ae785f3f2a0a958e26f460804c8ffc7
KingA2009 <ajamolidinov12@gmailc.com>
2023-11-19 19:15:46 +0500

    lesson _2 is done

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/KingA2009/online_lesson
+
+go 1.21.0
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ lesson_2/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test title", Desc: "Test description", Content: "Hello world "},
+	}
+	fmt.Println("Endpoint Hit: All articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func testPostArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Test Post endpoint worked")
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage endpoint Hit")
+}
+
+func handlerRequests() {
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8001", myRouter))
+}
+
+func main() {
+	handlerRequests()
+}
